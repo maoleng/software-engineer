35310a3e7d4a07f6314d55e29020e29962fb7443
Bùi Hữu Lộc <feature451@gmail.com>
2023-04-25 16:56:27 +0700

    feat: list & show product

--- source/Winform/UserControls/ManageProduct.Designer.cs
+++ source/Winform/UserControls/ManageProduct.Designer.cs
@@ -28,45 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tblProduct = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tblDiscounts = new System.Windows.Forms.DataGridView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imgImage = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rTxtDescription = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.slCategory = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tblProduct)).BeginInit();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblDiscounts)).BeginInit();
             this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgImage)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // tblProduct
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(21, 40);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(593, 299);
-            this.dataGridView1.TabIndex = 2;
+            this.tblProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblProduct.Location = new System.Drawing.Point(21, 40);
+            this.tblProduct.Name = "tblProduct";
+            this.tblProduct.RowHeadersWidth = 51;
+            this.tblProduct.RowTemplate.Height = 24;
+            this.tblProduct.Size = new System.Drawing.Size(593, 299);
+            this.tblProduct.TabIndex = 2;
+            this.tblProduct.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tblProduct_CellContentClick);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.dataGridView2);
-            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.tblDiscounts);
+            this.groupBox1.Controls.Add(this.tblProduct);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(15, 383);
             this.groupBox1.Name = "groupBox1";
@@ -75,15 +76,15 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Product List";
             // 
-            // dataGridView2
+            // tblDiscounts
             // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(632, 40);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.RowHeadersWidth = 51;
-            this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(270, 299);
-            this.dataGridView2.TabIndex = 3;
+            this.tblDiscounts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblDiscounts.Location = new System.Drawing.Point(632, 40);
+            this.tblDiscounts.Name = "tblDiscounts";
+            this.tblDiscounts.RowHeadersWidth = 51;
+            this.tblDiscounts.RowTemplate.Height = 24;
+            this.tblDiscounts.Size = new System.Drawing.Size(270, 299);
+            this.tblDiscounts.TabIndex = 3;
             // 
             // groupBox2
             // 
@@ -92,15 +93,15 @@
             this.groupBox2.Controls.Add(this.button3);
             this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.imgImage);
             this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.rTxtDescription);
             this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.slCategory);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.txtPrice);
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.txtName);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.Location = new System.Drawing.Point(15, 23);
             this.groupBox2.Name = "groupBox2";
@@ -109,106 +110,14 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Product Detail";
             // 
-            // textBox1
+            // button5
             // 
-            this.textBox1.Location = new System.Drawing.Point(280, 66);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 34);
-            this.textBox1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(275, 34);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 29);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Name";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(506, 34);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 29);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Price";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(511, 66);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(156, 34);
-            this.textBox2.TabIndex = 2;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(697, 66);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(205, 37);
-            this.comboBox1.TabIndex = 4;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(692, 34);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(110, 29);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Category";
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(280, 155);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(622, 107);
-            this.richTextBox1.TabIndex = 6;
-            this.richTextBox1.Text = "";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(275, 123);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(135, 29);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Description";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(21, 33);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(234, 229);
-            this.pictureBox1.TabIndex = 8;
-            this.pictureBox1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(217, 279);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 47);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Initialize";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(468, 279);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(125, 47);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Create";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(599, 279);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(118, 47);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "Update";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button5.Location = new System.Drawing.Point(723, 279);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(179, 47);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Edit Discount";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
@@ -219,14 +128,107 @@
             this.button4.Text = "Cancel";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // button3
             // 
-            this.button5.Location = new System.Drawing.Point(723, 279);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(179, 47);
-            this.button5.TabIndex = 13;
-            this.button5.Text = "Edit Discount";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button3.Location = new System.Drawing.Point(599, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 47);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(468, 279);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 47);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Create";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 47);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Initialize";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // imgImage
+            // 
+            this.imgImage.Location = new System.Drawing.Point(21, 33);
+            this.imgImage.Name = "imgImage";
+            this.imgImage.Size = new System.Drawing.Size(234, 229);
+            this.imgImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgImage.TabIndex = 8;
+            this.imgImage.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(275, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Description";
+            // 
+            // rTxtDescription
+            // 
+            this.rTxtDescription.Location = new System.Drawing.Point(280, 155);
+            this.rTxtDescription.Name = "rTxtDescription";
+            this.rTxtDescription.Size = new System.Drawing.Size(622, 107);
+            this.rTxtDescription.TabIndex = 6;
+            this.rTxtDescription.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(692, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Category";
+            // 
+            // slCategory
+            // 
+            this.slCategory.FormattingEnabled = true;
+            this.slCategory.Location = new System.Drawing.Point(697, 66);
+            this.slCategory.Name = "slCategory";
+            this.slCategory.Size = new System.Drawing.Size(205, 37);
+            this.slCategory.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(506, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Price";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(511, 66);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(156, 34);
+            this.txtPrice.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(275, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(280, 66);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 34);
+            this.txtName.TabIndex = 0;
             // 
             // ManageProduct
             // 
@@ -238,34 +240,35 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "ManageProduct";
             this.Size = new System.Drawing.Size(950, 750);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.ManageProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tblProduct)).EndInit();
             this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblDiscounts)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgImage)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView tblProduct;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView tblDiscounts;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox imgImage;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox rTxtDescription;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox slCategory;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtPrice;
     }
 }
--- source/Winform/UserControls/ManageProduct.cs
+++ source/Winform/UserControls/ManageProduct.cs
@@ -3,18 +3,97 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Winform.UserControls
 {
     public partial class ManageProduct : UserControl
     {
+        public const int GOOGLE = 1;
+        public const int SAMSUNG = 2;
+        public const int SONY = 3;
+        public const int NOKIA = 4;
+        public const int LG = 5;
+        public const int XIAOMI = 6;
+        public const int VIVO = 7;
+        public const int OPPO = 8;
+        public const int ONEPLUS = 9;
+        public const int HUAWEI = 10;
+
+        SoftwareEngineerEntities db = new SoftwareEngineerEntities();
+
         public ManageProduct()
         {
             InitializeComponent();
         }
+
+        private void ManageProduct_Load(object sender, EventArgs e)
+        {
+            tblProduct.DataSource = db.Products.ToList();
+            displayCategoryComboBox();
+            displayCategoryComboBox();
+        }
+
+        private void tblProduct_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = tblProduct.Rows[e.RowIndex];
+            int id = Convert.ToInt32(row.Cells[0].Value);
+            int category = Convert.ToInt32(row.Cells[1].Value);
+            string image = row.Cells[5].Value.ToString();
+
+            displayProductDiscount(id);
+            displayProductData(id, image);
+            slCategory.SelectedValue = category;
+        }
+
+        void displayCategoryComboBox()
+        {
+            var categoryNames = new[] {
+                new { CategoryId = GOOGLE, CategoryName = "GOOGLE" },
+                new { CategoryId = SAMSUNG, CategoryName = "SAMSUNG" },
+                new { CategoryId = SONY, CategoryName = "SONY" },
+                new { CategoryId = NOKIA, CategoryName = "NOKIA" },
+                new { CategoryId = LG, CategoryName = "LG" },
+                new { CategoryId = XIAOMI, CategoryName = "XIAOMI" },
+                new { CategoryId = VIVO, CategoryName = "VIVO" },
+                new { CategoryId = OPPO, CategoryName = "OPPO" },
+                new { CategoryId = ONEPLUS, CategoryName = "ONEPLUS" },
+                new { CategoryId = HUAWEI, CategoryName = "HUAWEI" },
+            };
+
+            slCategory.DataSource = categoryNames;
+            slCategory.DisplayMember = "CategoryName";
+            slCategory.ValueMember = "CategoryId";
+        }
+
+        void displayProductData(int id, string image)
+        {
+            var product = db.Products.Find(id);
+            txtName.Text = product.name;
+            txtPrice.Text = product.price.ToString();
+            rTxtDescription.Text = product.description;
+            using (var webClient = new System.Net.WebClient())
+            {
+                byte[] imageData = webClient.DownloadData(image);
+                using (var stream = new MemoryStream(imageData))
+                {
+                    imgImage.Image = Image.FromStream(stream);
+                }
+            }
+        }
+
+        void displayProductDiscount(int id)
+        {
+            var discounts = db.Discounts.Where(d => d.product_id == id)
+                .Select(x => new { Amount = x.need_amount, x.percent }).ToList();
+            tblDiscounts.DataSource = discounts;
+        }
+
     }
 }
