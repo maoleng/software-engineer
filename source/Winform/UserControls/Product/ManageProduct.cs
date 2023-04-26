ab7beb6fc808edc3c59ba8d66452bc13d773f871
Bùi Hữu Lộc <116434648+tanbaolailaptrinh@users.noreply.github.com>
2023-04-26 10:47:12 +0700

    move ManageProduct Form to /Product

--- source/Winform/UserControls/Product/ManageProduct.cs
+++ source/Winform/UserControls/Product/ManageProduct.cs
@@ -196,7 +196,7 @@ namespace Winform.UserControls
             db.Products.Add(new Product()
             {
                 category = Convert.ToInt32(slCategory.SelectedValue),
-                name = txtImage.Text,
+                name = txtName.Text,
                 price = Convert.ToInt32(txtPrice.Text),
                 description = rTxtDescription.Text,
                 image = txtImage.Text,
