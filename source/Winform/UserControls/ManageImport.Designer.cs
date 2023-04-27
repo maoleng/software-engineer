895d2753102cf15227c1ec0da08e362512d66a0f
Bùi Hữu Lộc <116434648+tanbaolailaptrinh@users.noreply.github.com>
2023-04-27 13:26:43 +0700

    list import & add product to import list

--- source/Winform/UserControls/ManageImport.Designer.cs
+++ source/Winform/UserControls/ManageImport.Designer.cs
@@ -28,32 +28,226 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tblImport = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtProductId = new System.Windows.Forms.TextBox();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tblAddedList = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.tblProduct = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.tblImport)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tblAddedList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblProduct)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tblImport
+            // 
+            this.tblImport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblImport.Location = new System.Drawing.Point(21, 40);
+            this.tblImport.Name = "tblImport";
+            this.tblImport.RowHeadersWidth = 51;
+            this.tblImport.RowTemplate.Height = 24;
+            this.tblImport.Size = new System.Drawing.Size(532, 299);
+            this.tblImport.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tblImport);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 376);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(918, 354);
+            this.groupBox1.TabIndex = 24;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Import List";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtProductId);
+            this.groupBox2.Controls.Add(this.btnImport);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtPrice);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtAmount);
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Controls.Add(this.tblAddedList);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtQuery);
+            this.groupBox2.Controls.Add(this.tblProduct);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(16, 23);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(918, 354);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Import Product";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 29);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "Product ID";
+            // 
+            // txtProductId
+            // 
+            this.txtProductId.Location = new System.Drawing.Point(21, 306);
+            this.txtProductId.Name = "txtProductId";
+            this.txtProductId.ReadOnly = true;
+            this.txtProductId.Size = new System.Drawing.Size(84, 34);
+            this.txtProductId.TabIndex = 35;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Enabled = false;
+            this.btnImport.Location = new System.Drawing.Point(559, 300);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(98, 47);
+            this.btnImport.TabIndex = 34;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(554, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 29);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "Added List";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(271, 274);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 29);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Price";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Enabled = false;
+            this.txtPrice.Location = new System.Drawing.Point(276, 306);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(127, 34);
+            this.txtPrice.TabIndex = 31;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 29);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Amount";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Enabled = false;
+            this.txtAmount.Location = new System.Drawing.Point(176, 306);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(89, 34);
+            this.txtAmount.TabIndex = 29;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Enabled = false;
+            this.btnAdd.Location = new System.Drawing.Point(409, 300);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(98, 47);
+            this.btnAdd.TabIndex = 28;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // tblAddedList
+            // 
+            this.tblAddedList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblAddedList.Location = new System.Drawing.Point(559, 75);
+            this.tblAddedList.Name = "tblAddedList";
+            this.tblAddedList.RowHeadersWidth = 51;
+            this.tblAddedList.RowTemplate.Height = 24;
+            this.tblAddedList.Size = new System.Drawing.Size(336, 194);
+            this.tblAddedList.TabIndex = 27;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(397, 340);
+            this.label1.Location = new System.Drawing.Point(16, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "import";
+            this.label1.Size = new System.Drawing.Size(89, 29);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Search";
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(111, 33);
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(216, 34);
+            this.txtQuery.TabIndex = 25;
+            // 
+            // tblProduct
+            // 
+            this.tblProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblProduct.Location = new System.Drawing.Point(21, 75);
+            this.tblProduct.Name = "tblProduct";
+            this.tblProduct.RowHeadersWidth = 51;
+            this.tblProduct.RowTemplate.Height = 24;
+            this.tblProduct.Size = new System.Drawing.Size(532, 194);
+            this.tblProduct.TabIndex = 24;
+            this.tblProduct.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tblProduct_CellContentClick);
             // 
             // ManageImport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
             this.Name = "ManageImport";
-            this.Size = new System.Drawing.Size(856, 696);
+            this.Size = new System.Drawing.Size(950, 750);
+            this.Load += new System.EventHandler(this.ManageImport_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tblImport)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tblAddedList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblProduct)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.DataGridView tblImport;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView tblProduct;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.DataGridView tblAddedList;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtProductId;
     }
 }
--- source/Winform/UserControls/ManageImport.cs
+++ source/Winform/UserControls/ManageImport.cs
@@ -12,9 +12,123 @@ namespace Winform.UserControls
 {
     public partial class ManageImport : UserControl
     {
+
+        SoftwareEngineerEntities db = new SoftwareEngineerEntities();
+        int productId = 0;
+        int importId = 0;
+        List<ImportProduct> importProducts = new List<ImportProduct>();
+
         public ManageImport()
         {
             InitializeComponent();
+        }
+
+        private void ManageImport_Load(object sender, EventArgs e)
+        {
+            reloadImportList();
+            reloadProductList();
+        }
+
+        void reloadProductList()
+        {
+            tblProduct.DataSource = db.Products.Select(p => new
+            {
+                p.id,
+                Name = p.name, 
+                Price = p.price
+            }).ToList();
+        }
+
+
+        void reloadImportList()
+        {
+            tblImport.DataSource = db.Imports.Select(p => new
+            {
+                p.id,
+                Total = p.product_price,
+                CreatedAt = p.created_at,
+            }).OrderByDescending(c => c.CreatedAt).ToList();
+        }
+
+        private void tblProduct_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = tblProduct.Rows[e.RowIndex];
+            int id = Convert.ToInt32(row.Cells["id"].Value);
+            productId = id;
+
+            txtProductId.Text = id.ToString();
+
+            ImportProduct importProduct = importProducts.Where(i => i.product_id == productId).SingleOrDefault();
+            txtAmount.Text = importProduct == null ? "0" : importProduct.amount.ToString();
+            txtPrice.Text = importProduct == null ? "0" : importProduct.price.ToString();
+
+            txtAmount.Enabled = true;
+            txtPrice.Enabled = true;
+            btnAdd.Enabled = true;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (productId == 0)
+            {
+                MessageBox.Show("Please choose a product");
+
+                return;
+            }
+            if (txtAmount.Text == "" || txtPrice.Text == "")
+            {
+                MessageBox.Show("Please fill amount and price");
+
+                return;
+            }
+            addProductToList();
+            updateTableAddedList();
+            toogleButtonImport();
+        }
+
+        void addProductToList()
+        {
+            ImportProduct importProduct = importProducts.Where(i => i.product_id == productId).SingleOrDefault();
+            int amount = Convert.ToInt32(txtAmount.Text);
+            if (importProduct == null)
+            {
+                if (amount == 0)
+                {
+                    return;
+                }
+                importProducts.Add(new ImportProduct
+                {
+                    product_id = productId,
+                    amount = amount,
+                    price = Convert.ToInt32(txtPrice.Text),
+                });
+            }
+            else
+            {
+                if (amount == 0)
+                {
+                    importProducts.Remove(importProduct);
+
+                    return;
+                }
+                importProduct.amount = amount;
+                importProduct.price = Convert.ToInt32(txtPrice.Text);
+            }
+        }
+
+        void updateTableAddedList()
+        {
+            tblAddedList.DataSource = importProducts.Select(i => new
+            {
+                PID = i.product_id,
+                Amount = i.amount,
+                Price = i.price
+            }).ToList();
+        }
+
+        void toogleButtonImport()
+        {
+            btnImport.Enabled = importProducts.Any();
         }
     }
 }
