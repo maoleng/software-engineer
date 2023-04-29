1f979527a23971aa415b2b9d764081b03da10d08
Bùi Hữu Lộc <91431461+maoleng@users.noreply.github.com>
2023-04-29 11:15:21 +0700

    feat: base controller & base view

---
+++ source/Webform/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Session = Microsoft.AspNetCore.Http.ISession;
+
+
+namespace Webform.Controllers
+{
+    abstract public class BaseController : Controller
+    {
+
+        protected Session session;
+
+		public override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			base.OnActionExecuting(filterContext);
+			session = HttpContext.Session;
+		}
+
+    
+    }
+}
