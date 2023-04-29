5ba8f102313281400bb3fd9e739d007f632c6cc2
Bùi Hữu Lộc <91431461+maoleng@users.noreply.github.com>
2023-04-29 14:22:20 +0700

    feat: redirectback, RedirectBackWithMessage

--- source/Webform/Controllers/BaseController.cs
+++ source/Webform/Controllers/BaseController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Data.Common;
+using System.Diagnostics;
+using Webform.Helpers;
+using Webform.Models;
 using Session = Microsoft.AspNetCore.Http.ISession;
 
 
@@ -7,8 +13,13 @@ namespace Webform.Controllers
 {
     abstract public class BaseController : Controller
     {
+		protected Session session;
+		public SoftwareEntities db;
 
-        protected Session session;
+		protected BaseController(SoftwareEntities context)
+		{
+			db = context;
+		}
 
 		public override void OnActionExecuting(ActionExecutingContext filterContext)
 		{
@@ -16,6 +27,23 @@ namespace Webform.Controllers
 			session = HttpContext.Session;
 		}
 
-    
+		protected Admin? Authed()
+		{
+			return session.GetObject<Admin>("authed") ?? null;
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+		public IActionResult RedirectBackWithMessage(string message)
+		{
+            TempData["message"] = message;
+
+            return this.RedirectBack();
+        }
     }
 }
---
+++ source/Webform/Helpers/RedirectExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webform.Helpers
+{
+    public static class RedirectExtension
+    {
+        public static IActionResult RedirectBack(this Controller controller)
+        {
+            if (controller.HttpContext.Request.Headers["Referer"].ToString() != "")
+            {
+                return controller.Redirect(controller.HttpContext.Request.Headers["Referer"].ToString());
+            }
+            else
+            {
+                return controller.RedirectToAction("Index", "Home");
+            }
+        }
+    }
+
+
+}
+
