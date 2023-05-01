5d152c21dd0d5610fea0ee2222509e3f786856be
Bùi Hữu Lộc <91431461+maoleng@users.noreply.github.com>
2023-05-01 17:07:06 +0700

    feat: manage admin

--- source/Webform/Controllers/AdminController.cs
+++ source/Webform/Controllers/AdminController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
+using System.Reflection;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Web.UI;
 using Webform.Models;
 
 namespace Webform.Controllers
@@ -13,10 +17,86 @@ namespace Webform.Controllers
 
         [HttpGet]
         [Route("/hrm")]
-        public ActionResult Index()
+        public ActionResult Index(string created_at, string q, int page = 1)
         {
+            int pageSize = 20;
+
+            var builder = db.Admins.AsQueryable();
+            if (!string.IsNullOrEmpty(created_at))
+            {
+                var split = created_at.Split(',');
+                if (split.Length == 2)
+                {
+                    var start = DateTime.Parse(split[0]);
+                    var end = DateTime.Parse(split[1]);
+                    builder = builder.Where(o => o.created_at >= start && o.created_at <= end);
+                }
+            }
+            if (!string.IsNullOrEmpty(q))
+            {
+                builder = builder.Where(o =>
+                    o.name.Contains(q) ||
+                    o.email.ToString().Contains(q) ||
+                    o.created_at.ToString().Contains(q)
+                );
+            }
+            var admins = builder.OrderByDescending(o => o.created_at)
+                                .Skip((page - 1) * pageSize)
+                                .Take(pageSize)
+                                .ToList();
+
+            ViewBag.Admins = admins;
+            ViewBag.Paginate = new Paginate(builder.Count(), pageSize);
+
             return View("~/Views/Admin/Index.cshtml");
         }
+        
+
+        [HttpPost]
+        [Route("/hrm")]
+        public ActionResult Store(FormCollection data)
+        {
+            if (string.IsNullOrEmpty(data["name"]) || string.IsNullOrEmpty(data["email"]))
+            {
+                TempData["error"] = "Field must not be empty";
+
+                return RedirectBack();
+            }
+
+            db.Admins.Add(new Admin()
+            {
+                name = data["name"],
+                email = data["email"],
+                password = BCrypt.Net.BCrypt.HashPassword(data["email"]),
+                is_admin_master = false,
+                created_at = DateTime.Now,
+            });
+            db.SaveChanges();
+            TempData["success"] = "Create accountant successfully";
+
+            return RedirectBack();
+        }
+
+
+        [HttpPost]
+        [Route("/hrm")]
+        public ActionResult Cancel(int id)
+        {
+            Admin admin = db.Admins.Find(id);
+            if (admin.is_admin_master)
+            {
+                TempData["error"] = "Can not handle on admin master";
+
+                return RedirectBack();
+            }
+            admin.active = !admin.active;
+            db.SaveChanges();
+
+            TempData["success"] = admin.active ? "Locked successfully" : "Unlock successfully";
+
+            return RedirectBack();
+        }
+
 
     }
 }
