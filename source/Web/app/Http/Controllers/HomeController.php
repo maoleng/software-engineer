6995e80b24f7e9cd45ba7f03de36191386cb8360
Bùi Hữu Lộc <91431461+maoleng@users.noreply.github.com>
2023-05-03 17:19:18 +0700

    feat: search & filter

--- source/Web/app/Http/Controllers/HomeController.php
+++ source/Web/app/Http/Controllers/HomeController.php
@@ -9,9 +9,48 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::query()->paginate(9);
+        $query = $request->all();
+        $q = $query['q'] ?? null;
+        $sort = $query['sort'] ?? null;
+        $price = $query['price'] ?? null;
+        $category = $query['category'] ?? null;
+
+
+
+        $builder = Product::query();
+        if ($q !== null) {
+            $builder->where(function ($query) use ($q) {
+                $query->orWhere('name', 'LIKE', "%$q%")
+                    ->orWhere('description', 'LIKE', "%$q%");
+            });
+        }
+        if ($price) {
+            $builder->where(function ($query) use ($price) {
+                switch ($price) {
+                    case '1':
+                        $query->where('price', '<=', $price);
+                        break;
+                    case '2':
+                        $query->orWhere('price', '>=', 1000000)->where('price', '<=', 4000000);
+                        break;
+                    case '3':
+                        $query->orWhere('price', '>=', 4000000)->where('price', '<=', 10000000);
+                        break;
+                    case '4':
+                        $query->where('price', '>=', 10000000);
+                        break;
+                }
+            });
+        }
+        if ($category !== null) {
+            $builder->where('category', array_search($category, ProductCategory::getDescriptions(), true));
+        }
+        if (in_array($sort, ['acs', 'desc'])) {
+            $builder->orderBy('price', $sort);
+        }
+        $products = $builder->paginate(9);
         $categories = ProductCategory::getDescriptions();
 
         return view('index', [
--- source/Web/resources/views/index.blade.php
+++ source/Web/resources/views/index.blade.php
@@ -30,33 +30,34 @@
                         <div class="multi-range-price">
                             <h6 class="filter-title mt-0">Multi Range</h6>
                             <ul class="list-unstyled price-range" id="price-range">
+                                @php ($price = request()->get('price'))
                                 <li>
                                     <div class="form-check">
-                                        <input type="radio" id="priceAll" name="price-range" class="form-check-input" checked />
+                                        <input type="radio" id="priceAll" name="price-range" class="form-check-input" data-value="all" {{ $price === 'all' ? 'checked' : '' }} />
                                         <label class="form-check-label" for="priceAll">All</label>
                                     </div>
                                 </li>
                                 <li>
                                     <div class="form-check">
-                                        <input type="radio" id="priceRange1" name="price-range" class="form-check-input" />
+                                        <input type="radio" id="priceRange1" name="price-range" class="btn-price form-check-input" data-value="1" {{ $price === '1' ? 'checked' : '' }} />
                                         <label class="form-check-label" for="priceRange1">&lt;=1,000,000</label>
                                     </div>
                                 </li>
                                 <li>
                                     <div class="form-check">
-                                        <input type="radio" id="priceRange2" name="price-range" class="form-check-input" />
+                                        <input type="radio" id="priceRange2" name="price-range" class="btn-price form-check-input" data-value="2" {{ $price === '2' ? 'checked' : '' }} />
                                         <label class="form-check-label" for="priceRange2">1,000,000 - 4,000,000</label>
                                     </div>
                                 </li>
                                 <li>
                                     <div class="form-check">
-                                        <input type="radio" id="priceARange3" name="price-range" class="form-check-input" />
+                                        <input type="radio" id="priceARange3" name="price-range" class="btn-price form-check-input" data-value="3" {{ $price === '3' ? 'checked' : '' }} />
                                         <label class="form-check-label" for="priceARange3">4,000,000 - 10,000,000</label>
                                     </div>
                                 </li>
                                 <li>
                                     <div class="form-check">
-                                        <input type="radio" id="priceRange4" name="price-range" class="form-check-input" />
+                                        <input type="radio" id="priceRange4" name="price-range" class="btn-price form-check-input" data-value="4" {{ $price === '4' ? 'checked' : '' }} />
                                         <label class="form-check-label" for="priceRange4">&gt;= 10,000,000</label>
                                     </div>
                                 </li>
@@ -68,14 +69,14 @@
                         <div class="brands">
                             <h6 class="filter-title">Category</h6>
                             <ul class="list-unstyled brand-list">
-                                @foreach ($categories as $key => $category)
-                                <li>
-                                    <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="{{ $key }}" checked/>
-                                        <label class="form-check-label" for="productBrand1">{{ $category }}</label>
-                                    </div>
-                                    <span>746</span>
-                                </li>
+                                @foreach ($categories as $category)
+                                    <li>
+                                        <div data-value="{{ $category }}" class="btn-category form-check">
+                                            <input id="category_{{ $category }}" {{ request()->get('category') === $category ? 'checked' : '' }} type="radio" class="form-check-input"/>
+                                            <label class="form-check-label" for="productBrand1">{{ $category }}</label>
+                                        </div>
+                                        <span>746</span>
+                                    </li>
                                 @endforeach
                             </ul>
                         </div>
@@ -109,11 +110,20 @@
                             <div class="view-options d-flex">
                                 <div class="btn-group dropdown-sort">
                                     <button type="button" class="btn btn-outline-primary dropdown-toggle me-1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <span class="active-sorting">Featured</span>
+                                        <span class="active-sorting">
+                                            @php ($sort_type = request()->get('sort'))
+                                            @if ($sort_type === null)
+                                                Sort
+                                            @elseif($sort_type === 'asc')
+                                                Lowest
+                                            @elseif($sort_type === 'desc')
+                                                Highest
+                                            @endif
+                                        </span>
                                     </button>
                                     <div class="dropdown-menu">
-                                        <a class="dropdown-item" href="#">Lowest</a>
-                                        <a class="dropdown-item" href="#">Highest</a>
+                                        <a data-value="desc" class="btn-sort dropdown-item" href="#">Lowest</a>
+                                        <a data-value="asc" class="btn-sort dropdown-item" href="#">Highest</a>
                                     </div>
                                 </div>
                                 <div class="btn-group" role="group">
@@ -138,7 +148,7 @@
                 <div class="row mt-1">
                     <div class="col-sm-12">
                         <div class="input-group input-group-merge">
-                            <input type="text" class="form-control search-product" id="shop-search" placeholder="Search Product" aria-label="Search..." aria-describedby="shop-search" />
+                            <input value="{{ request()->get('q') }}" type="text" class="form-control search-product" id="i-search" placeholder="Search Product" aria-label="Search..." aria-describedby="shop-search" />
                             <span class="input-group-text"><i data-feather="search" class="text-muted"></i></span>
                         </div>
                     </div>
@@ -152,12 +162,13 @@
                     <div class="card ecommerce-card">
                     <div class="item-img text-center">
                         <a href="{{ route('product.show', ['product' => $product]) }}">
-                            <img class="img-fluid card-img-top" src="{{ $product->image }}" alt="img-placeholder" /></a>
+                            <img class="img-fluid card-img-top" src="{{ $product->image }}" alt="img-placeholder" />
+                        </a>
                     </div>
                     <div class="card-body">
                         <div class="item-wrapper">
                             <div class="item-rating">
-
+                                {{ $product->categoryName }}
                             </div>
                             <div>
                                 <h6 class="item-price">{{ prettyPrice($product->price) }}</h6>
@@ -165,7 +176,7 @@
                         </div>
                         <h6 class="item-name">
                             <a class="text-body" href="{{ route('product.show', ['product' => $product]) }}">{{ $product->name }}</a>
-                            <span class="card-text item-company">By <a href="#" class="company-name">{{ $product->categoryName }}</a></span>
+                            <span class="card-text item-company">By <a href="#" class="company-name"></a></span>
                         </h6>
                         <p class="card-text item-description">
                             {{ $product->description }}
@@ -215,12 +226,21 @@
     <script src={{ asset('app-assets/js/scripts/pages/app-ecommerce.js') }}></script>
     <script>
         $(document).ready(function() {
-            if (localStorage.getItem('show') === '1') {
+            $('#i-search').on('keypress',function(e) {
+                if(e.which === 13) {
+                    handle('q', $(this).val())
+                }
+            })
+            $('.btn-sort').on('click',function() {
+                handle('sort', $(this).data('value'))
+            })
+            $('.btn-price').on('click', function () {
+                handle('price', $(this).data('value'))
+            })
+            $('.btn-category').on('click', function () {
+                handle('category', $(this).data('value'))
+            })
 
-
-                localStorage.removeItem('show')
-
-            }
 
 
             $('.btn-add_to_cart').on('click', function () {
@@ -236,6 +256,36 @@
                     console.log(e)
                 });
             })
+
+            function handle(param_name, value)
+            {
+                let url = window.location.href.split('?')[0];
+                const urlSearchParams = new URLSearchParams(window.location.search);
+                const params = Object.fromEntries(urlSearchParams.entries());
+                const param_names = Object.keys(params)
+
+                if (params[param_name] !== undefined) {
+                    let query_string = ''
+                    let first = true
+                    if (param_names.length > 0) {
+                        param_names.forEach(function (name) {
+                            if (name !== param_name) {
+                                query_string += first ? '?' : '&'
+                                query_string += `${name}=${params[name]}`
+                                first = false
+                            }
+                        })
+                        query_string += query_string === '' ? `?${param_name}=${value}` : `&${param_name}=${value}`
+                    } else {
+                        query_string += `?${param_name}=${value}`
+                    }
+                    window.location.href = url + query_string
+
+                    return
+                }
+                const link_char = param_names.length > 0 ? '&' : '?'
+                window.location.href = window.location.href + link_char + `${param_name}=` + value
+            }
         })
     </script>
 @endsection
